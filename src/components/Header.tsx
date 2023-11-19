3dde5f36f5a96671f210689501a3f424d322ee72
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-19 18:44:00 +0000

    use StartTimer and StopTimer Actions in the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,19 @@ import Button from './UI/Button.tsx';
 import { useTimersContext } from '../store/timers-context.tsx';
 
 export default function Header() {
-  const timersCtx = useTimersContext();
+	const timersCtx = useTimersContext();
 
-  return (
-    <header>
-      <h1>ReactTimer</h1>
+	return (
+		<header>
+			<h1>ReactTimer</h1>
 
-      <Button>{timersCtx.isRunning ? 'Stop' : 'Start'} Timers</Button>
-    </header>
-  );
+			<Button
+				onClick={
+					timersCtx.isRunning ? timersCtx.stopTimers : timersCtx.startTimers
+				}
+			>
+				{timersCtx.isRunning ? 'Stop' : 'Start'} Timers
+			</Button>
+		</header>
+	);
 }
